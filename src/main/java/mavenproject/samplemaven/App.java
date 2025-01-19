872421b2b4dbe52b5a3b098e59fabb013cbfea1c
muzammil-ibrahim <afreedmohd927@gmail.com>
2025-01-19 19:04:36 +0530

    sample maven java

---
+++ src/main/java/mavenproject/samplemaven/App.java
@@ -0,0 +1,13 @@
+package mavenproject.samplemaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
